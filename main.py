9bf738d2beed681c1f55797a1a5a2fa6f1cba631
root <root@MSI>
2026-07-01 16:02:27 +0100

    Finalize model and SHAP analysis

--- main.py
+++ main.py
@@ -12,3 +12,25 @@ model = joblib.load("src/churn_model.pkl")
 @app.get("/")
 def home():
     return {"message": "Customer Churn API running"}
+
+
+from pydantic import BaseModel
+
+class CustomerInput(BaseModel):
+    Gender: str
+    Senior_Citizen: str
+    Partner: str
+    Dependents: str
+    Tenure_Months: int
+    Internet_Service: str
+    Contract: str
+    Payment_Method: str
+    Monthly_Charges: float
+    Total_Charges: float
+    
+@app.post("/predict")
+def predict(customer: CustomerInput):
+
+    return {
+        "message": "Prediction endpoint working"
+    }
